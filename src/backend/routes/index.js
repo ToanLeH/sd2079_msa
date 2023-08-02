250048e5671c4488fb17d0d9a077fe599e5031de
root <root@VNNOT01969.localdomain>
2023-08-02 21:55:08 +0700

    add custom metrics

--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -2,14 +2,27 @@ const express = require("express");
 const serverResponses = require("../utils/helpers/responses");
 const messages = require("../config/messages");
 const { Todo } = require("../models/todos/todo");
+const Prometheus = require('prom-client');
 
 const routes = (app) => {
   const router = express.Router();
+  const collectDefaultMetrics = Prometheus.collectDefaultMetrics;
+  const counter = new Prometheus.Counter({
+    name: 'number_of_post_request_hit',
+    help: 'sample custom metrics to track number of post request hit',
+  });
+  collectDefaultMetrics({
+    //labels: { NODE_APP_INSTANCE: process.env.NODE_APP_INSTANCE },
+    labels: { NODE_APP_INSTANCE: 'backend' },
+  });
 
   router.post("/todos", (req, res) => {
     const todo = new Todo({
       text: req.body.text,
     });
+
+    //sample custom metrics
+    counter.inc();
 
     todo
       .save()
@@ -21,6 +34,16 @@ const routes = (app) => {
       });
   });
 
+  router.get('/metrics', function(req, res)
+  {
+      res.setHeader('Content-Type',Prometheus.register.contentType)
+
+      Prometheus.register.metrics().then(data => {
+        //serverResponses.sendSuccess(res, messages.SUCCESSFUL, data);
+        res.end(data);
+      })
+  });
+
   router.get("/", (req, res) => {
     Todo.find({}, { __v: 0 })
       .then((todos) => {
@@ -30,6 +53,7 @@ const routes = (app) => {
         serverResponses.sendError(res, messages.BAD_REQUEST, e);
       });
   });
+
 
   //it's a prefix before api it is useful when you have many modules and you want to
   //differentiate b/w each module you can use this technique
